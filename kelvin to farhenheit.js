eb147578a86df500659d82d9fc31b6890fb7b8d1
omama17 <63242788+omama17@users.noreply.github.com>
2020-06-01 19:59:54 +0530

    Update kelvin to farhenheit.js

--- kelvin to farhenheit.js
+++ kelvin to farhenheit.js
@@ -1,7 +1,7 @@
-/*let kelvinTemp = 301;
+let kelvinTemp = 301;
 let celsiusTemp = kelvinTemp - 273.15;
 let fahrenheitTemp = celsiusTemp * (9/5) + 32;
 //document.write(fahrenheitTemp);
 let temperature = Math.floor(fahrenheitTemp);
 document.write(`The temperature is ${temperature} degrees Fahrenheit.`);
-*/
+
