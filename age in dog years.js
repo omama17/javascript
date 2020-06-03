57d89c4d1530f1ffeeb25323be0c25efa915f63e
omama17 <63242788+omama17@users.noreply.github.com>
2020-06-03 14:06:30 +0530

    Add files via upload

---
+++ age in dog years.js
@@ -0,0 +1,5 @@
+var myAge = 19;
+var firstYears = 2 * 10.5;
+var laterYears = (myAge - 2) * 4;
+var myAgeInDogYears = firstYears + laterYears;
+document.write(`my age in dog years is: ${myAgeInDogYears}.`);
